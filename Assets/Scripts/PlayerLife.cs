fd37240de68109a285bbac3aaf2bf55d1e112380
Janice <janiceeminnzly@gmail.com>
2026-04-17 04:24:45 +0800

    Added 1. Added traps 2. Added death animation 3. Added restart level after death

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // when the player collides with an object tagged "Trap", the player will die
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    // body of the player will be static and the death animation will be triggered
+    private void Die()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        animator.SetTrigger("death");
+    }
+
+    // restart the level after a delay of 1 second (to allow the death animation to play)
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
